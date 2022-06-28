5bcaa0d688552f99e608a45c72e1208da331cab5
Hassan Ayomide <funyem1710@gmail.com>
2022-06-28 18:43:51 +0100

    def last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last = int(repr(number)[-1])
+    if (number > 0):
+        return (last)
+    elif (number == 0):
+        return (last)
+    else:
+        return (last * -1)
