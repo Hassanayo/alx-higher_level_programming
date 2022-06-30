e86d9c4d562c1248775614290cffb9eb9aa295e5
Hassan Ayomide <funyem1710@gmail.com>
2022-06-30 15:54:35 +0100

    who are u

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     name = dir(hidden_4)
     for i in name:
         if (i[:2] == "__"):
-            continue
+            pass
         else:
-            print(i)
+            print("{}".format(i))
