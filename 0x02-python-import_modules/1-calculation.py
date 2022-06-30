f6dfc321702ab33acc0a8b94ea106ebda874cc3d
Hassan Ayomide <funyem1710@gmail.com>
2022-06-30 10:03:49 +0100

    calculation

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,7 @@
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+print(str(a), " + ", str(b), " = ", add(a, b))
+print(str(a), " - ", str(b), " = ", sub(a, b))
+print(str(a), " * ", str(b), " = ", mul(a, b))
+print(str(a), " / ", str(b), " = ", div(a, b))
