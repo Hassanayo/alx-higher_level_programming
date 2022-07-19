d229d57203a5973d98ed81f19d37a454cebf4109
Hassan Ayomide <funyem1710@gmail.com>
2022-07-19 12:45:22 +0100

    size init

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-
+"""class square"""
 
 class Square:
-    """
-        Args:
-        Attributes:
-    """
+    """Square"""
     def __init__(self, size):
+        """Square
+        Args:
+            size(int): size of the square
+        """
         self.__size = size
